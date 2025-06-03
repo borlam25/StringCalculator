12a87f96c99bfd0e267e6326beeebc4a010f222b
praneeth <praneeth.borlam@acicorp.com>
2025-06-03 15:37:13 +0530

    test: accept newline as valid delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,7 @@ class StringCalculator
   def self.add(input)
     return 0 if input.strip.empty?
   
+    input = input.gsub("\n", ",")
     numbers = input.split(",").map(&:to_i)
     numbers.sum
   end  
